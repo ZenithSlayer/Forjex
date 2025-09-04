c205940d64950ae220adc8023ce99b5eac329232
ZenithSlayer <miguellurk@gmail.com>
2025-09-03 21:56:08 -0300

    finished footer and about page

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,9 @@
 <footer>
-    <h1>Forjex is free and pretty not very good</h1>
-    <div>
+    <div class="text">
+        <h1>Forjex is free and pretty not very good</h1>
+        <p>get a load of this Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos perspiciatis quam doloremque, aliquam eveniet enim in obcaecati consequuntur, rerum deleniti minus eos voluptates sed totam? Reiciendis veniam voluptatem officia quidem!</p>
+    </div>
+    <div class="link">
         <a href="https://instagram.com"><img src="imgs/temp.jpg" alt=""><p>Gmail</p></a>
         <a href="https://instagram.com"><img src="imgs/temp.jpg" alt=""><p>Instagram</p></a>
         <a href="https://instagram.com"><img src="imgs/temp.jpg" alt=""><p>Github</p></a>
--- includes/pages/about.php
+++ includes/pages/about.php
@@ -1,1 +1,21 @@
-<p>Cock</p>
+<div class="block">
+    <img src="imgs/temp.jpg" alt="">
+    <div class="text">
+        <p>Who am i</p>
+        <p>Take this shitload of Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse cupiditate, dignissimos ea amet, quae nihil aliquam est sed, totam itaque fugit quaerat! Sunt autem excepturi, obcaecati alias aperiam ducimus ipsa?</p>
+    </div>
+</div>
+<div class="block">
+    <img src="imgs/temp.jpg" alt="">
+    <div class="text">
+        <p>Why does this exist</p>
+        <p>Take this shitload of Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse cupiditate, dignissimos ea amet, quae nihil aliquam est sed, totam itaque fugit quaerat! Sunt autem excepturi, obcaecati alias aperiam ducimus ipsa?</p>
+    </div>
+</div>
+<div class="block">
+    <img src="imgs/temp.jpg" alt="">
+    <div class="text">
+        <p>Why Forjex</p>
+        <p>Take this shitload of Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse cupiditate, dignissimos ea amet, quae nihil aliquam est sed, totam itaque fugit quaerat! Sunt autem excepturi, obcaecati alias aperiam ducimus ipsa?</p>
+    </div>
+</div>
