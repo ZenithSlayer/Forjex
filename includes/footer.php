feb4ddd42ef9c89a9bca4bfee8d4e71409c07873
ZenithSlayer <miguellurk@gmail.com>
2025-09-04 16:36:32 -0300

    added logos to footer

--- includes/footer.php
+++ includes/footer.php
@@ -4,8 +4,8 @@
         <p>get a load of this Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos perspiciatis quam doloremque, aliquam eveniet enim in obcaecati consequuntur, rerum deleniti minus eos voluptates sed totam? Reiciendis veniam voluptatem officia quidem!</p>
     </div>
     <div class="link">
-        <a href="https://instagram.com"><img src="imgs/temp.jpg" alt=""><p>Gmail</p></a>
-        <a href="https://instagram.com"><img src="imgs/temp.jpg" alt=""><p>Instagram</p></a>
-        <a href="https://instagram.com"><img src="imgs/temp.jpg" alt=""><p>Github</p></a>
+        <a href="https://instagram.com"><img src="imgs/Gmail.png" alt=""><p>Gmail</p></a>
+        <a href="https://instagram.com"><img src="imgs/Instagram.png" alt=""><p>Instagram</p></a>
+        <a href="https://instagram.com"><img src="imgs/Github.png" alt=""><p>Github</p></a>
     </div>
 </footer>
