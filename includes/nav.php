17c20595663ae18af5be0a76e6d09795440510ee
ZenithSlayer <miguellurk@gmail.com>
2025-09-05 11:57:05 -0300

    added login to nav

--- includes/nav.php
+++ includes/nav.php
@@ -10,4 +10,8 @@
         <a data-active="about" href="?page=about">About</a>
         <a data-active="contact" href="?page=contact">Contact</a>
     </div>
+    <div class="login">
+        <a data-active="login" href="?page=login&state=login">Login</a>
+        <a data-active="register" href="?page=login&state=register">Register</a>
+    </div>
 </nav>
