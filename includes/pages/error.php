52541ebc5cce0f18878519341ed7af1daad43e20
ZenithSlayer <miguellurk@gmail.com>
2025-09-08 17:10:01 -0300

    added error page

--- includes/pages/error.php
+++ includes/pages/error.php
@@ -1,1 +1,11 @@
-<p>Fuck</p>
+<div class="e404">
+    <div class="text">
+        <p>The lands which you look for are long gone</p>
+        <p>(or it could have been moved)</p>
+        <img src="./imgs/404wizard.gif">
+        <h1>404 page not found</h1>
+    </div>
+    <div class="link">
+        <a href="?page=home"><p>Go back</p></a>
+    </div>
+</div>
