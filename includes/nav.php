00cdbaf00c43b582f2aab4b3b15fdbe444101899
ZenithSlayer <miguellurk@gmail.com>
2025-09-10 21:59:13 -0300

    changed style from navbar

--- includes/nav.php
+++ includes/nav.php
@@ -1,3 +1,7 @@
+<?php
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+?>
+
 <nav class="main-nav">
     <div class="logo">
         <a href="?page=home">
@@ -7,19 +11,20 @@
     </div>
 
     <div class="links">
-        <a href="?page=home">Home</a>
-        <a href="?page=about">About</a>
-        <a href="?page=contact">Contact</a>
+        <a href="?page=home" class="<?php echo ($page == 'home') ? 'active' : '' ?>">Home</a>
+        <a href="?page=about" class="<?php echo ($page == 'about') ? 'active' : '' ?>">About</a>
+        <a href="?page=contact" class="<?php echo ($page == 'contact') ? 'active' : '' ?>">Contact</a>
+    <div>
 
+    </div>
+    <div class="speciallink">
         <?php if (isset($_SESSION['user_id'])): ?>
-            <div class="loginnav">
-                <a href="?page=logout">Logout</a>
-            </div>
+            <a href="?page=createchar">Characters</a>
+            <a href="?page=logout">Logout</a>
         <?php else: ?>
-            <div class="loginnav">
-                <a href="?page=login&state=login">Login</a>
-                <a href="?page=login&state=register">Register</a>
-            </div>
+            <a href="?page=login&state=login">Login</a>
+            <a href="?page=login&state=register">Register</a>
         <?php endif; ?>
+    </div>
     </div>
 </nav>
