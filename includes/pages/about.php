7a556666d9c8e5576da57ce6d94aa920913b59ce
ZenithSlayer <miguellurk@gmail.com>
2025-09-18 22:51:39 -0300

    added more standard styling

--- includes/pages/about.php
+++ includes/pages/about.php
@@ -1,21 +1,23 @@
-<div class="block">
-    <img src="imgs/about_1.jpg" alt="">
-    <div class="text">
-        <p>Who am i</p>
-        <p>I'm some nerd that likes rpg and programing, i also need a good grade, so why not make a website that does all that (at least i think i'll get a good grade).</p>
+<div class="about">
+    <div class="block">
+        <img src="imgs/about_1.jpg" alt="">
+        <div class="text">
+            <p>Who am i</p>
+            <p>I'm some nerd that likes rpg and programing, i also need a good grade, so why not make a website that does all that (at least i think i'll get a good grade).</p>
+        </div>
     </div>
-</div>
-<div class="block">
-    <img src="imgs/about_2.jpg" alt="">
-    <div class="text">
-        <p>Why does this exist</p>
-        <p>As i said early, i need a good grade for collage, and i also really enjoy playing D&D, but i hate updating my dnd sheet, mostly because it now has the thinkness of toilet paper that you got from the bathroom from a gas-station in the middle of nowhere, so with this tool, i can store and update my characters easily.</p>
+    <div class="block">
+        <img src="imgs/about_2.jpg" alt="">
+        <div class="text">
+            <p>Why does this exist</p>
+            <p>As i said early, i need a good grade for collage, and i also really enjoy playing D&D, but i hate updating my dnd sheet, mostly because it now has the thinkness of toilet paper that you got from the bathroom from a gas-station in the middle of nowhere, so with this tool, i can store and update my characters easily.</p>
+        </div>
     </div>
-</div>
-<div class="block">
-    <img src="imgs/about_3.jpg" alt="">
-    <div class="text">
-        <p>Why Forjex</p>
-        <p>honestly? i liked how it sounded, and because heros are created in the heat of battle just like swords that are shaped in the anvil of a forge, i decided to make the icon a anvil, and the name forjex because we are in the internet and everything is indexed (also, the j is just for it to sound better in the promunciation)</p>
+    <div class="block">
+        <img src="imgs/about_3.jpg" alt="">
+        <div class="text">
+            <p>Why Forjex</p>
+            <p>honestly? i liked how it sounded, and because heros are created in the heat of battle just like swords that are shaped in the anvil of a forge, i decided to make the icon a anvil, and the name forjex because we are in the internet and everything is indexed (also, the j is just for it to sound better in the promunciation)</p>
+        </div>
     </div>
 </div>
--- includes/pages/charlist.php
+++ includes/pages/charlist.php
@@ -10,25 +10,23 @@ $stmt->execute([$user_id]);
 $characters = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<div class="charlist">
-
-<?php if (count($characters) === 0): ?>
-    <div class="nochar">
-        <p>You have no characters created yet.</p>
-        <a href="?page=createchar">Create Character</a>
-    </div>
-<?php else: ?>
-    <p>Your Characters</p>
-    <ul>
-        <?php foreach ($characters as $char): ?>
-            <li>
-                <strong><?= htmlspecialchars($char['charname']) ?></strong>
-                (Level <?= $char['charlv'] ?> <?= htmlspecialchars($char['charclass']) ?> - <?= htmlspecialchars($char['charrace']) ?>)<br>
-                Stats: STR <?= $char['str_stat'] ?>, DEX <?= $char['dex_stat'] ?>, CON <?= $char['con_stat'] ?>,
-                INT <?= $char['int_stat'] ?>, WIS <?= $char['wis_stat'] ?>, CHA <?= $char['cha_stat'] ?>
-            </li><br>
-        <?php endforeach; ?>
-    </ul>
-<?php endif; ?>
-
+<div class="chatlist">
+    <?php if (count($characters) === 0): ?>
+        <div class="nochar">
+            <p>You have no characters created yet.</p>
+            <a href="?page=createchar">Create Character</a>
+        </div>
+    <?php else: ?>
+        <p>Your Characters</p>
+        <ul>
+            <?php foreach ($characters as $char): ?>
+                <li>
+                    <strong><?= htmlspecialchars($char['charname']) ?></strong>
+                    (Level <?= $char['charlv'] ?> <?= htmlspecialchars($char['charclass']) ?> - <?= htmlspecialchars($char['charrace']) ?>)<br>
+                    Stats: STR <?= $char['str_stat'] ?>, DEX <?= $char['dex_stat'] ?>, CON <?= $char['con_stat'] ?>,
+                    INT <?= $char['int_stat'] ?>, WIS <?= $char['wis_stat'] ?>, CHA <?= $char['cha_stat'] ?>
+                </li><br>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
 </div>
